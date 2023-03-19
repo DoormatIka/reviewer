5163d53768417762c5397795d38d3d03a0305bc3
DoormatIka <trashcanartistlily@gmail.com>
2023-03-19 13:45:05 +0800

    fixed overflow issues with Card.svelte

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,7 +1,8 @@
 
 <div class="group
-  sm:flex sm:items-center sm:flex-col sm:gap-1
-  p-7 shadow-md rounded-xl bg-white
+  p-7 shadow-md rounded-xl bg-white flex-wrap
 ">
-  <slot />
+  <div class="flex justify-content break-words justify-center flex-col">
+    <slot />
+  </div>
 </div>
--- src/routes/subjects/[subject]/+page.svelte
+++ src/routes/subjects/[subject]/+page.svelte
@@ -1,21 +1,22 @@
 <script>
   /** @type {import("./$types").PageData} */
   export let data;
+
+  import Card from "$lib/Card.svelte";
+  const logo = "https://media.discordapp.net/attachments/1061320413373808741/1085617532804812970/favicon.png?width=560&height=560";
 </script>
 
 <p>{data.post?.description}</p>
 <p>Note to the dev: Transfer the data.js file into a database.</p>
 
-<div class="flex justify-center">
-  <div class="grid grid-cols-2 grid-rows-2 sm:grid-cols-3 md:grid-cols-4">
+<div class="flex justify-center align-items p-5">
+  <div class="grid grid-cols-2 grid-rows-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
     {#each data.post?.topics as { topic, description }}
-      <div class="
-        sm:flex sm:items-center sm:flex-col sm:gap-1
-        p-7 shadow-md rounded-xl bg-white
-      ">
+      <Card>
+        <img src={logo} alt="" class=" max-h-32 object-contain" />
         <h3>{topic}</h3>
         <p>{description}</p>
-      </div>
+      </Card>
     {/each}
   </div>
 </div>
