ba1791fa768cb7b3dc6e416eaa76af70303f621d
DoormatIka <trashcanartistlily@gmail.com>
2023-04-01 16:15:43 +0800

    netlify-adapter split set to false

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
       edge: false,
-      split: true
+      split: false,
     }),
     paths: {
       base: ""
